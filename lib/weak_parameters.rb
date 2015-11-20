edc326f0d407885c91f4211ad6abf07ab9b20680
Isamu Arimoto <isamu@to-kyo.to>
2015-11-21 05:40:56 +0900

    add active_support/core_ext/object/blank

--- lib/weak_parameters.rb
+++ lib/weak_parameters.rb
@@ -1,6 +1,6 @@
 require "active_support/hash_with_indifferent_access"
 require "active_support"
-#require "action_dispatch"
+require 'active_support/core_ext/object/blank'
 
 
 require "weak_parameters/base_validator"
